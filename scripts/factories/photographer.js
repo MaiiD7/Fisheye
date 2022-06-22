c4aa6add72d727787fc5ba94b0cc83208da01e63
Mairo Dronsart <mairo.dronsart@csgroup.eu>
2022-06-22 18:33:24 +0200

    commit

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,9 +1,14 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+
+        const section = document.createElement('section');
+
+        const link = document.createElement('a');
+        link.href = '../../photographer.html';
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
@@ -11,7 +16,22 @@ function photographerFactory(data) {
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        link.appendChild(article);
+
+        const paragraphe = document.createElement('article');
+        const h3 = document.createElement('h3');
+        h3.textContent = `${city}, ${country}`;
+        const p1 = document.createElement('p');
+        p1.textContent = tagline;
+        const span = document.createElement('span');
+        span.textContent = `${price}€/jour`;
+        paragraphe.appendChild(h3);
+        paragraphe.appendChild(p1);
+        paragraphe.appendChild(span);
+
+        section.appendChild(link);
+        section.appendChild(paragraphe);
+        return (section);
     }
     return { name, picture, getUserCardDOM }
 }
